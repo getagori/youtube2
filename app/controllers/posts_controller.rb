bb88c0f12fa4b65c26f0232b0eba5e2c97ea6b32
koudai <57044519+getagori@users.noreply.github.com>
2020-03-08 14:19:16 +0900

    編集削除機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,25 @@ class PostsController < ApplicationController
 
   def create
     Post.create(post_params)
+    redirect_to :action => "index"
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to :action => "index"
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.delete
+    redirect_to :action => "index"
+  end
+
 
 
   private
